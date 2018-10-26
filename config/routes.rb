3d6dfd6abe05274caa73c5484f049581b9c253aa
MOHD FAIZ <tenkois@gmail.com>
2018-10-26 12:55:06 +0800

    create two pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
+  get 'home/team'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
